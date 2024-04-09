f5c6e6bbd8db30c2abf919847a195103710d2a68
Stefan Bauer <stefanjakob.bauer@gmail.com>
2024-04-09 09:27:51 +0200

    fix: update allowedFileTypes for xlsx files

--- services/app/src/lib/upload/cloud/s3/config.ts
+++ services/app/src/lib/upload/cloud/s3/config.ts
@@ -4,7 +4,8 @@ export const allowedFileTypes = [
     // 'video/mp4',
     // 'video/quicktime'
     'text/csv',
-    'application/vnd.ms-excel'
+    'application/vnd.ms-excel',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 ];
 
 export const maxFileSize = 1024 * 1024 * 10; // 10MB
