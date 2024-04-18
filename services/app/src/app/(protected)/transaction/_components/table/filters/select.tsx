b450d5ae10152325146a2f91325f7ad6551e040d
Stefan Bauer <stefanjakob.bauer@gmail.com>
2024-04-18 20:48:46 +0200

    feat: add react-query to fetch filter options

--- services/app/src/app/(protected)/transaction/_components/table/filters/select.tsx
+++ services/app/src/app/(protected)/transaction/_components/table/filters/select.tsx
@@ -1,4 +1,8 @@
+import { transactionActions } from '@/actions';
 import { SelectFilter } from '@/components/data-table/filters';
+import { useFetch } from '@/hooks/action/useFetch';
+import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
 import { useTransactionTableFilteringStore } from '../store';
 import type { TTransactionFilterKeys } from './types';
@@ -18,25 +22,34 @@ export const TransactionTableSelectFilter: React.FC<Props> = ({
     );
     const selectedFilters = filters[filterKey] || [];
 
+    const { data, refetch } = useQuery({
+        queryKey: ['filterOptions', filterKey],
+        queryFn: () => transactionActions.listFilterOptions({ filterKey }),
+        enabled: false
+    });
+
     // todo type narrowing
     if (!Array.isArray(selectedFilters)) {
         return null;
     }
 
-    const options = [
-        {
-            label: 'Hotel',
-            value: '83dde3c5-b108-44a3-8a7d-7612e7450d6a'
-        }
-    ];
+    console.log({ data });
+
+    // const options = [
+    //     {
+    //         label: 'Hotel',
+    //         value: '83dde3c5-b108-44a3-8a7d-7612e7450d6a'
+    //     }
+    // ];
 
     return (
         <SelectFilter
             filterKey={filterKey}
             filterLabel={filterLabel}
-            options={options}
+            options={data?.success || []}
             selectedValues={selectedFilters}
             filterFn={setFilter}
+            filterFetchFn={refetch}
         />
     );
 };
