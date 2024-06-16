8f3460e8942a0908bdf46d2d52350b5026c9647e
Stefan Bauer <stefanjakob.bauer@gmail.com>
2024-06-16 13:35:33 +0200

    feat: major update to transaction view sheet

--- services/app/src/features/transaction/components/view-transaction/view-transaction-sheet.tsx
+++ services/app/src/features/transaction/components/view-transaction/view-transaction-sheet.tsx
@@ -1,15 +1,25 @@
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle
-} from '@/components/ui/sheet';
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { Separator } from '@/components/ui/separator';
+import { Sheet, SheetContent, SheetFooter } from '@/components/ui/sheet';
+import { LabelCard } from '@/features/label/components/label-card';
 import { useGetTransaction } from '@/features/transaction/api/get-transaction';
-import { Amount } from '@/features/transaction/components/utils';
 import { storeViewUpdateTransactionSheet } from '@/features/transaction/store/view-update-transaction-sheet';
+import { SelectLabelSchema } from '@db/schema';
 import dayjs from 'dayjs';
-import { LuCalendar } from 'react-icons/lu';
+import { Copy } from 'lucide-react';
+import { LuCalendar, LuMoreVertical } from 'react-icons/lu';
+import { z } from 'zod';
+
+import { TransactionType } from '../table/utils';
 
 interface Props {}
 
@@ -22,62 +32,244 @@ export const ViewTransactionSheet: React.FC<Props> = () => {
     if (!data) return null;
 
     const {
+        title,
         accountAmount,
         accountCurrency,
         spendingAmount,
         spendingCurrency,
-        label
+        userAmount,
+        userCurrency,
+        type,
+        label,
+        note,
+        country,
+        city,
+        createdAt,
+        account
     } = data;
+
+    const dateFormatted = dayjs(data?.date).format('ddd, DD-MMM YYYY');
+    const createdAtFormatted = dayjs(createdAt).format('DD-MMM YYYY');
 
     return (
         <Sheet open={isOpen} onOpenChange={handleClose}>
-            <SheetContent size="lg">
-                <SheetHeader className="mb-8">
-                    {data?.title && <SheetTitle>{data.title}</SheetTitle>}
-                    <SheetDescription className="flex items-center">
-                        <LuCalendar className="h-4 w-4 mr-1" />
-                        {dayjs(data?.date).format('ddd, DD-MMM YYYY')}
-                    </SheetDescription>
-                </SheetHeader>
-                <div className="flex space-x-14 mb-10">
-                    <div>
-                        <div className="text-muted-foreground text-sm">
-                            Spending Amount
-                        </div>
-                        <Amount
-                            amount={spendingAmount}
-                            currency={spendingCurrency}
-                        />
+            <SheetContent
+                size="lg"
+                className="p-0 overflow-scroll space-y-4"
+                hideCloseButton={true}
+            >
+                <CardHeader className="flex flex-row items-start bg-muted/50 border-b">
+                    <div className="grid gap-0.5">
+                        <CardTitle className="group flex items-center gap-2 text-lg">
+                            {title}
+                            <Button
+                                size="icon"
+                                variant="outline"
+                                className="h-6 w-6 opacity-0 transition-opacity group-hover:opacity-100"
+                            >
+                                <Copy className="h-3 w-3" />
+                                <span className="sr-only">Edit Title</span>
+                            </Button>
+                        </CardTitle>
+                        <CardDescription className="flex items-center">
+                            <LuCalendar className="h-4 w-4 mr-1.5" />
+                            <time dateTime={dateFormatted}>
+                                {dateFormatted}
+                            </time>
+                        </CardDescription>
                     </div>
-                    <div>
-                        <div className="text-muted-foreground text-sm">
-                            Account Amount
+                    <div className="ml-auto flex items-center gap-1">
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button
+                                    size="icon"
+                                    variant="outline"
+                                    className="h-8 w-8"
+                                >
+                                    <LuMoreVertical className="h-3.5 w-3.5" />
+                                    <span className="sr-only">More</span>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuItem>Edit</DropdownMenuItem>
+                                <DropdownMenuItem>Export</DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>Trash</DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                </CardHeader>
+
+                <div className="px-6 text-sm">
+                    {/* Amount */}
+                    <div className="grid gap-3">
+                        <div className="font-semibold">Amount Details</div>
+                        <ul className="grid gap-3">
+                            <li className="flex items-center justify-between">
+                                <span className="text-muted-foreground">
+                                    Transaction
+                                </span>
+                                <Amount
+                                    amount={spendingAmount}
+                                    currency={spendingCurrency}
+                                />
+                            </li>
+                            <li className="flex items-center justify-between">
+                                <span className="text-muted-foreground">
+                                    Booked
+                                </span>
+                                <Amount
+                                    amount={accountAmount}
+                                    currency={accountCurrency}
+                                />
+                            </li>
+                            <li className="flex items-center justify-between">
+                                <span className="text-muted-foreground">
+                                    Budget
+                                </span>
+                                <Amount
+                                    amount={userAmount}
+                                    currency={userCurrency}
+                                />
+                            </li>
+                        </ul>
+                    </div>
+                    <Separator className="my-4" />
+
+                    <div className="grid grid-cols-2 gap-4">
+                        {/* Account */}
+                        <div className="grid gap-3">
+                            <div className="font-semibold">Account</div>
+                            <div className="grid gap-0.5 not-italic text-muted-foreground">
+                                <span>
+                                    {account.name} - {account.bank.bank.name}
+                                </span>
+                            </div>
                         </div>
-                        <Amount
-                            amount={accountAmount}
-                            currency={accountCurrency}
-                        />
+                        {/* Type */}
+                        <div className="space-y-3">
+                            <div className="font-semibold">Type</div>
+                            {/* <div className="grid gap-0.5 not-italic text-muted-foreground"> */}
+                            <TransactionType type={type} />
+                            {/* </div> */}
+                        </div>
+                    </div>
+
+                    <Separator className="my-4" />
+
+                    {/* Info */}
+                    <div className="grid gap-3">
+                        <div className="font-semibold">
+                            Additional Information
+                        </div>
+                        <dl className="grid gap-3">
+                            <div className="flex items-center justify-between">
+                                <dt className="text-muted-foreground">
+                                    Location
+                                </dt>
+                                <dd>Zurich, CH</dd>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <dt className="text-muted-foreground">Tags</dt>
+                                <dd className="space-x-1">
+                                    <Badge>Travel</Badge>
+                                    <Badge>Travel</Badge>
+                                    <Badge>Travel</Badge>
+                                    <Badge>Travel</Badge>
+                                </dd>
+                            </div>
+                        </dl>
+                    </div>
+                    <Separator className="my-4" />
+
+                    {/* Note */}
+                    <div className="grid gap-3">
+                        <div className="font-semibold ">Note</div>
+                        <span className="text-justify">
+                            Die Elf von Julian Nagelsmann agiert extrem geduldig
+                            und erhöht nun auf 2:0. Deutschland wartet, bis die
+                            Bravehearts sich aus der Reserve locken lassen, und
+                            spielt dann explosionsartig nach vorne. Gündoğan
+                            findet Havertz mit einem Zuckerpass, der es selbst
+                            machen könnte, aber lieber wartet, bis Musiala
+                            nachrückt. Das Zuspiel kommt an und der Münchner
+                            Spieler lässt sich nicht zweimal bitten.
+                            Kompromisslos nagelt er die Kugel aus elf Metern mit
+                            rechts oben links in den Winkel. Nichts zu halten
+                            für Gunn.
+                        </span>
                     </div>
                 </div>
-                <div className="mb-6">
-                    <div className="text-muted-foreground text-sm">Label</div>
-                    {label ? label.name : 'No Label'}
-                </div>
-                <div className="flex space-x-10">
-                    <div>
-                        <div className="text-muted-foreground text-sm">
-                            City
-                        </div>
-                        {data.city ? data.city : 'No Label'}
+
+                <SheetFooter className="border-t bg-muted/50 px-6 py-3 absolute bottom-0 w-full">
+                    <div className="text-xs text-muted-foreground">
+                        Imported{' '}
+                        <time dateTime={createdAtFormatted}>
+                            {createdAtFormatted}
+                        </time>
                     </div>
-                    <div>
-                        <div className="text-muted-foreground text-sm">
-                            Country
-                        </div>
-                        {data.country ? data.country : 'No Label'}
-                    </div>
-                </div>
+                </SheetFooter>
             </SheetContent>
         </Sheet>
     );
+};
+
+const Info: React.FC<{ label: string; info?: string | null }> = ({
+    label,
+    info
+}) => {
+    if (!info) return null;
+
+    return (
+        <div>
+            <div className="text-muted-foreground text-sm">{label}</div>
+            <span className="text">{info}</span>
+        </div>
+    );
+};
+
+const Amount: React.FC<{ amount: number; currency: string }> = ({
+    amount,
+    currency
+}) => {
+    return (
+        <span>
+            {currency}{' '}
+            {(amount / 100).toLocaleString('en-US', {
+                maximumFractionDigits: 2,
+                minimumFractionDigits: 2,
+                currency: currency
+            })}
+        </span>
+    );
+};
+
+const AmountCard: React.FC<{ amount: number; currency: string }> = ({
+    amount,
+    currency
+}) => {
+    const decimale = Number(String(amount).slice(-2));
+
+    return (
+        <div>
+            <span className="text-4xl font-bold">
+                {Math.trunc(amount / 100).toLocaleString('en-US', {
+                    maximumFractionDigits: 0,
+                    minimumFractionDigits: 0,
+                    currency: currency
+                })}
+            </span>
+            {decimale === 0 ? <span></span> : <span>.{decimale}</span>}
+
+            <span className="text-sm ml-1">{currency}</span>
+        </div>
+    );
+};
+
+const Label: React.FC<{ label: z.infer<typeof SelectLabelSchema> | null }> = ({
+    label
+}) => {
+    if (!label) return null;
+
+    return <LabelCard label={label} />;
 };
