2e031f467a2930e05c61b71d5fac693b6e790aa3
Stefan Bauer <stefanjakob.bauer@gmail.com>
2024-10-17 19:33:11 +0200

    fix: disable auto migration through __abstract__ = True

--- services/parser/app/db/model.py
+++ services/parser/app/db/model.py
@@ -5,6 +5,7 @@ from .client import Base
 
 class Transaction(Base):
     __tablename__ = "transaction"
+    __abstract__ = True
 
     id = Column(String, primary_key=True)
     userId = Column(String)
