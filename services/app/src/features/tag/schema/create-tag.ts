8092ab57214ca3deb8314d7bb3df784ae8f84df0
Stefan Bauer <stefanjakob.bauer@gmail.com>
2024-10-21 16:41:24 +0200

    fix: schema import

---
+++ services/app/src/features/tag/schema/create-tag.ts
@@ -0,0 +1,18 @@
+import { tag } from '@/db/schema';
+import { createId } from '@paralleldrive/cuid2';
+import { createInsertSchema } from 'drizzle-zod';
+import { z } from 'zod';
+
+// export const CreateTagSchema = z.object({
+//     color: z.string().optional(),
+//     name: z.string().trim().min(1).max(255)
+// });
+
+export const CreateTagSchema = createInsertSchema(tag).pick({
+    userId: true,
+    color: true,
+    name: true,
+    description: true
+});
+
+// export type TCreateTagValues = z.input<typeof CreateTagSchema>;
