43e6e65a0343084fb1761959864d7eac442b8fd8
Stefan Bauer <stefanjakob.bauer@gmail.com>
2024-10-23 13:24:15 +0200

    fix: error middleware

--- services/app/src/server/middleware/validation.ts
+++ services/app/src/server/middleware/validation.ts
@@ -1,4 +1,5 @@
 import { createMiddleware } from 'hono/factory';
+import type { StatusCode } from 'hono/utils/http-status';
 
 /**
  * Middleware to handle validation errors
@@ -6,21 +7,35 @@ import { createMiddleware } from 'hono/factory';
 export const validationError = createMiddleware(async (c, next) => {
     await next();
 
-    if (c.res.status === 400) {
-        const res = await c.res.json();
-        if ('success' in res && 'error' in res && res.success === false) {
-            const firstIssue = res.error?.issues[0];
-
-            // todo log and create custom Validation error
-
-            c.res = c.json(
-                {
-                    error: 'Validation error',
-                    detail: firstIssue?.message || 'Unknown error',
-                    path: firstIssue?.path || 'Unknown path'
-                },
-                400
-            );
-        }
+    // if status is not 400 or above, skip
+    if (c.res.status < 400) {
+        return;
     }
+
+    const status = c.res.status as StatusCode;
+    const res = await c.res.json();
+
+    if (
+        typeof res === 'object' &&
+        'success' in res &&
+        'error' in res &&
+        res.success === false
+    ) {
+        const firstIssue = res.error?.issues[0];
+        // build new response
+        c.res = c.json(
+            {
+                error: 'Validation Error',
+                detail: firstIssue?.message || 'Unknown error',
+                path: firstIssue?.path || 'Unknown path'
+            },
+            400
+        );
+        return;
+    } // return;
+
+    // todo logging
+
+    // build new response
+    c.res = c.json(res, status);
 });
