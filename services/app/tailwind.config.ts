316c762efa02e3cfda612c9dbffeb2c30cc97a62
Stefan Bauer <stefanjakob.bauer@gmail.com>
2025-02-23 20:27:15 +0100

    chore: improve tailwind config

--- services/app/tailwind.config.ts
+++ services/app/tailwind.config.ts
@@ -1,14 +1,10 @@
+import { act } from 'react';
 import type { Config } from 'tailwindcss';
 import colors from 'tailwindcss/colors';
 
 const config = {
     darkMode: ['class'],
-    content: [
-        './pages/**/*.{ts,tsx}',
-        './components/**/*.{ts,tsx}',
-        './app/**/*.{ts,tsx}',
-        './src/**/*.{ts,tsx}'
-    ],
+    content: ['./src/**/*.{ts,tsx}'],
     prefix: '',
     theme: {
         container: {
@@ -19,39 +15,97 @@ const config = {
             }
         },
         colors: {
-            ...colors,
+            background: 'hsl(var(--background))', // background color
+            foreground: 'hsl(var(--foreground))', // text color
+            border: colors.gray[300],
+            input: colors.gray[300],
+            ring: colors.gray[300],
+            neutral: {
+                DEFAULT: colors.gray[300]
+            },
+
+            popover: {
+                DEFAULT: 'hsl(var(--popover))',
+                foreground: 'hsl(var(--popover-foreground))'
+            },
+
+            // primary: {
+            //     lighter: 'oklch(var(--primary-100)) /* oklch(94% 0.035 298) */',
+            //     light: 'oklch(var(--primary-300)) /* oklch(80% 0.25 298) */',
+            //     DEFAULT: 'oklch(var(--primary-500)) /* oklch(58% 0.25 298) */',
+            //     dark: 'oklch(var(--primary-600)) /* oklch(48% 0.25 298) */',
+            //     darker: 'oklch(var(--primary-700)) /* oklch(40% 0.25 298) */'
+            // },
             primary: {
-                DEFAULT: 'hsl(var(--primary))',
-                foreground: 'hsl(var(--primary-foreground))'
+                50: 'oklch(97% 0.0108 298)',
+                100: 'oklch(90% 0.02 298)',
+                200: 'oklch(80% 0.05 298)',
+                300: 'oklch(70% 0.12 298)',
+                400: 'oklch(60% 0.19 298)',
+                DEFAULT: 'oklch(53% 0.27 298)',
+                600: 'oklch(43% 0.19 298)',
+                700: 'oklch(35% 0.12 298)',
+                800: 'oklch(25% 0.05 298)',
+                900: 'oklch(15% 0.0898 298)',
+                950: 'oklch(10% 0.0236 298)'
+                // 50: 'oklch(97.78% 0.0108 298)',
+                // 100: 'oklch(93.56% 0.0321 298)',
+                // 200: 'oklch(88.11% 0.0609 298)',
+                // 300: 'oklch(82.67% 0.0908 298)',
+                // 400: 'oklch(74.22% 0.1398 298)',
+                // DEFAULT: 'oklch(64.78% 0.25 298)',
+                // 600: 'oklch(57.33% 0.1299 298)',
+                // 700: 'oklch(46.89% 0.1067 298)',
+                // 800: 'oklch(39.44% 0.0898 298)'
             },
-            border: 'hsl(var(--border))',
-            input: 'hsl(var(--input))',
-            ring: 'hsl(var(--ring))',
-            background: 'hsl(var(--background))',
-            foreground: 'hsl(var(--foreground))',
             secondary: {
-                DEFAULT: 'hsl(var(--secondary))',
-                foreground: 'hsl(var(--secondary-foreground))'
+                DEFAULT: 'hsl(var(--secondary-500))',
+                foreground: 'hsl(var(--background))',
+                background: {
+                    DEFAULT: 'hsl(var(--secondary-500))',
+                    hover: 'hsl(var(--secondary-300))',
+                    active: 'hsl(var(--secondary-600))',
+                    disabled: 'hsl(var(--secondary-100))'
+                }
             },
-            destructive: {
-                DEFAULT: 'hsl(var(--destructive))',
-                foreground: 'hsl(var(--destructive-foreground))'
+            accent: {
+                DEFAULT: 'hsl(var(--accent-500))',
+                foreground: 'hsl(var(--accent-600))',
+                background: {
+                    DEFAULT: 'hsl(var(--accent-500))',
+                    hover: 'hsl(var(--accent-300))',
+                    active: 'hsl(var(--accent-600))',
+                    disabled: 'hsl(var(--accent-100))'
+                }
+            },
+            // oklch(var(--dn-accent-300, 0.762 0.177 190.000) / <alpha-value>)
+
+            card: {
+                DEFAULT: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))'
             },
             muted: {
                 DEFAULT: 'hsl(var(--muted))',
                 foreground: 'hsl(var(--muted-foreground))'
             },
-            accent: {
-                DEFAULT: 'hsl(var(--accent))',
-                foreground: 'hsl(var(--accent-foreground))'
+            destructive: {
+                DEFAULT: 'hsl(var(--destructive))',
+                foreground: 'hsl(var(--destructive-foreground))'
             },
-            popover: {
-                DEFAULT: 'hsl(var(--popover))',
-                foreground: 'hsl(var(--popover-foreground))'
+            warning: {
+                DEFAULT: 'oklch(58% 0.25 105)'
             },
-            card: {
-                DEFAULT: 'hsl(var(--card))',
-                foreground: 'hsl(var(--card-foreground))'
+            success: {
+                '500': 'oklch(58% 0.25 135)'
+            },
+            error: {
+                DEFAULT: 'oklch(58% 0.25 20)'
+            },
+            info: {
+                DEFAULT: 'oklch(58% 0.25 267)'
+            },
+            sidebar: {
+                foreground: 'hsl(var(--sidebar-foreground))'
             }
         },
         extend: {
@@ -85,21 +139,21 @@ const config = {
                 'accordion-down': 'accordion-down 0.2s ease-out',
                 'accordion-up': 'accordion-up 0.2s ease-out',
                 'caret-blink': 'caret-blink 1.25s ease-out infinite'
-            },
-            colors: {
-                sidebar: {
-                    DEFAULT: 'hsl(var(--sidebar-background))',
-                    foreground: 'hsl(var(--sidebar-foreground))',
-                    primary: 'hsl(var(--sidebar-primary))',
-                    'primary-foreground':
-                        'hsl(var(--sidebar-primary-foreground))',
-                    accent: 'hsl(var(--sidebar-accent))',
-                    'accent-foreground':
-                        'hsl(var(--sidebar-accent-foreground))',
-                    border: 'hsl(var(--sidebar-border))',
-                    ring: 'hsl(var(--sidebar-ring))'
-                }
             }
+            // colors: {
+            //     sidebar: {
+            //         DEFAULT: 'hsl(var(--sidebar-background))',
+            //         foreground: 'hsl(var(--sidebar-foreground))',
+            //         primary: 'hsl(var(--sidebar-primary))',
+            //         'primary-foreground':
+            //             'hsl(var(--sidebar-primary-foreground))',
+            //         accent: 'hsl(var(--sidebar-accent))',
+            //         'accent-foreground':
+            //             'hsl(var(--sidebar-accent-foreground))',
+            //         border: 'hsl(var(--border))',
+            //         ring: 'hsl(var(--sidebar-ring))'
+            //     }
+            // }
         }
     },
     plugins: [require('tailwindcss-animate')]
