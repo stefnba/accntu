6007aae799ea0f2381b0bccb567dfe2819434cf4
Stefan Bauer <stefanjakob.bauer@gmail.com>
2025-02-28 23:06:24 +0100

    feat: add more allowed methods

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,3 +3,6 @@ import { handle } from 'hono/vercel';
 
 export const GET = handle(app);
 export const POST = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
+export const PUT = handle(app);
