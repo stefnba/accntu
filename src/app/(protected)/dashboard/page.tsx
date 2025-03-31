239b655fdef5340317a4a7c720165de06ad4cb74
Stefan Bauer <stefanjakob.bauer@gmail.com>
2025-03-31 19:55:56 +0900

    fix: padding top

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function DashboardPage() {
         <>
             <div className="flex flex-1 flex-col">
                 <div className="@container/main flex flex-1 flex-col gap-2">
-                    <div className="flex flex-col md:gap-6 md:py-6">
+                    <div className="flex flex-col md:gap-6">
                         <SectionCards />
                         <div className="">{/* <ChartAreaInteractive /> */}</div>
                         <DataTable data={data} />
