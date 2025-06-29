6fec896ee8baaeb3e1614063e92187e49abf0228
Stefan Bauer <stefanjakob.bauer@gmail.com>
2025-06-29 21:22:07 +0200

    feat: add CardHeader component

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -21,3 +21,44 @@ export const PageHeader: React.FC<PageHeaderProps> = ({
         </div>
     );
 };
+
+export interface CardHeaderProps {
+    title: string;
+    description?: string | React.ReactElement | null;
+    actionBar?: React.ReactNode;
+    icon?: React.ReactNode;
+}
+
+/**
+ * Card header
+ */
+export const CardHeader: React.FC<CardHeaderProps> = ({
+    title,
+    description,
+    actionBar: ActionBar,
+    icon,
+}) => {
+    return (
+        <div className="bg-white border border-gray-200 rounded-2xl shadow-sm">
+            <div className="p-6">
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-6">
+                        {icon && (
+                            <div className="size-20 rounded-2xl flex items-center justify-center shadow-sm border">
+                                {icon}
+                            </div>
+                        )}
+                        <div>
+                            <h1 className="text-3xl font-semibold text-gray-900 mb-2">{title}</h1>
+                            <div className="flex items-center gap-4 text-gray-600 text-sm">
+                                {description && <Description>{description}</Description>}
+                            </div>
+                        </div>
+                    </div>
+
+                    {ActionBar && <div className="ml-auto">{ActionBar} </div>}
+                </div>
+            </div>
+        </div>
+    );
+};
