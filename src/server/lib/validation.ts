7b2b365a6e3bbfbf50a54f6c93b3e236f53a816f
Stefan Bauer <stefanjakob.bauer@gmail.com>
2025-09-04 12:20:18 +0200

    migrate: update zValidator to use ZodType in v4

--- src/server/lib/validation.ts
+++ src/server/lib/validation.ts
@@ -1,14 +1,14 @@
 import { handleZodError } from '@/server/lib/error/handler';
 import { zValidator as zv } from '@hono/zod-validator';
 import type { ValidationTargets } from 'hono';
-import { ZodSchema } from 'zod';
+import { ZodType } from 'zod';
 
 /**
  * Centralized Zod validator that throws a ZodError when validation fails and hence falls back to our custom error handler
  *
  * This is used to validate the request body, query, cookie and params in the routes
  */
-export const zValidator = <T extends ZodSchema, Target extends keyof ValidationTargets>(
+export const zValidator = <T extends ZodType, Target extends keyof ValidationTargets>(
     target: Target,
     schema: T
 ) =>
