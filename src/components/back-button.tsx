4d75171c7893ba7b4e7a75e8a6f9ecd829c80fb1
Stefan Bauer <stefanjakob.bauer@gmail.com>
2025-10-23 15:12:05 +0200

    refactor: enhance BackButton component to support onClick prop and path navigation - Updated BackButtonProps to include an onClick handler for more flexible usage. - Refactored the onClick logic to handle both path navigation and custom click actions.

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -6,20 +6,37 @@ import { cn } from '@/lib/utils';
 import { IconArrowLeft } from '@tabler/icons-react';
 import { useRouter } from 'next/navigation';
 
-interface BackButtonProps {
+interface BackButtonPropsPath {
     className?: string;
     path?: RoutePath;
 }
 
-export const BackButton = ({ className, path }: BackButtonProps) => {
+interface BackButtonPropsOnClick {
+    className?: string;
+    onClick: () => void;
+}
+
+export const BackButton = (props: BackButtonPropsPath | BackButtonPropsOnClick) => {
     const router = useRouter();
+
+    const handleClick = () => {
+        if ('path' in props) {
+            if (props.path) {
+                router.push(props.path);
+            } else {
+                router.back();
+            }
+        } else if ('onClick' in props && typeof props.onClick === 'function') {
+            props.onClick();
+        }
+    };
 
     return (
         <Button
-            className={cn('cursor-pointer', className)}
+            className={cn('cursor-pointer', props.className)}
             variant="outline"
             size="sm"
-            onClick={() => (path ? router.push(path) : router.back())}
+            onClick={handleClick}
         >
             <IconArrowLeft className="h-4 w-4" />
             Back
